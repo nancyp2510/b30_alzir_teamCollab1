9a28247bcb8c4603d35bb894ae28ec060df1b6ce
zulyamanafli <manaflizulya@gmail.com>
2023-09-14 16:55:09 -0700

    created task1 & task2 (Java practice tasks)

---
+++ src/week01_zulya/evenOrOdd.java
@@ -0,0 +1,22 @@
+package week01_zulya;
+
+public class evenOrOdd {
+
+        public static void main(String[] args) {
+
+       identify(6);
+
+        }
+
+
+        public static void identify(int number) {
+            if (number % 2 == 0){
+                System.out.println("Even");
+            }else{
+                System.out.println("Odd");
+            }
+
+        }
+    }
+
+
