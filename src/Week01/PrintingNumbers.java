d3131f9d6fe8921309a94669afdc9c51b36ff2c5
omids <omidsahrifi079@gmail.com>
2023-10-04 21:25:03 -0700

    Created printingNumbers class

---
+++ src/Week01/PrintingNumbers.java
@@ -0,0 +1,20 @@
+package Week01;
+
+public class PrintingNumbers {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 31; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
