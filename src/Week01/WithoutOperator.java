5b021aa48f39250bcaa7f567b3bcce8a62c35294
omids <omidsahrifi079@gmail.com>
2023-10-04 21:26:03 -0700

    Created withoutOperator class

---
+++ src/Week01/WithoutOperator.java
@@ -0,0 +1,23 @@
+package Week01;
+
+import java.util.Scanner;
+
+public class WithoutOperator {
+
+    public static void main(String[] args) {
+
+        int num1 ,num2 , numbers;
+        int result = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first number");
+        num1 = input.nextInt();
+
+        System.out.println("Enter your second number");
+        num2 = input.nextInt();
+
+
+        System.out.println("Result is : "+Math.floorDiv(num1,num2));
+    }
+}
