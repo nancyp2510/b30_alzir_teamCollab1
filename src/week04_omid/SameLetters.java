84bc6acbe1954478014126f8e2daa1f630b6cd72
omids <omidsahrifi079@gmail.com>
2023-10-19 04:38:37 -0700

    Created Week04 Java coding task classes.

---
+++ src/week04_omid/SameLetters.java
@@ -0,0 +1,22 @@
+package week04_omid;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class SameLetters {
+    public static void main(String[] args) {
+
+        System.out.println("Same(\"abc\", \"cab\") = " + Same("abc", "cab"));
+
+    }
+        public static boolean Same(String str1, String str2) {
+            str1 = new TreeSet<String>(Arrays.asList( str1.split("") ) ).toString( );
+            str2 = new TreeSet<String>(Arrays.asList( str2.split("") ) ).toString( );
+            return str1.equals(str2);
+        }
+
+
+
+
+
+}
